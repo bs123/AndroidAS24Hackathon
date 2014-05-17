ff54322419ca6c6bb083f6d40e055feaed826127
Arian Avini <arian.avini@gmail.com>
2014-05-17 13:09:32 +0200

    android UI arian

--- src/com/ccuhack24/cargame/Screens/MapScreen/MapScreen.java
+++ src/com/ccuhack24/cargame/Screens/MapScreen/MapScreen.java
@@ -8,6 +8,8 @@ import com.ccuhack24.cargame.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.view.Display;
 import android.view.Menu;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -24,8 +26,26 @@ public class MapScreen extends Activity {
 
 	// get grid layout
 	theGrid = (RelativeLayout) this.findViewById(R.id.mapScreen_grid);
-	theGrid.addView(new PaintableGridView(this));
+	
+	// we need to find out the screen dimensions for the calculation
+	Display display = getWindowManager().getDefaultDisplay();
+	Point size = new Point();
+	display.getSize(size);
+	int screenWidth = size.x;
+	int screenHeight = size.y;
+	
+	// we also give the playing field matrix we get from the engine
+	// the playing field is a 2-dimensional array which has the value of the team in each cell
+	int[][] theField = new int[17][17];
+	
+	// fake a field
+	for (int i =0;i<theField.length;i++)
+	    for (int j=0;j<theField.length;j++)
+		theField[i][j] = (i+j) % 5;
+	
 	// attach view to activity
+	theGrid.addView(new PaintableGridView(this, screenWidth, screenHeight, theField));
+	
 	
     }
 
